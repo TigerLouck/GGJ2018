3f3badd2a2545b7f80d5eb855a8b4f265c76e0b4
David Liu (RIT Student) <dxl4041@ad.rit.edu>
2018-01-27 00:35:04 -0500

    Made Sand Puzzle Scripts

---
+++ GlobalGameJam2018/Assets/Scripts/Sand/SandBagHold.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * David Liu
+ * Sand Bag Hold
+ * Handles when the player picks up the sand bag with the string.
+ */
+public class SandBagHold : MonoBehaviour {
+
+    // Variables
+    public GameObject puzzleMaster;
+    public bool isHeld = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Interact()
+    {
+        isHeld = true;
+        puzzleMaster.SendMessage("HoldBag");
+    }
+}

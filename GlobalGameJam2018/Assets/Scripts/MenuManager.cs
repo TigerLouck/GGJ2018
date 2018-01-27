14c28f865ba26672833c40da96a80cebe52bb068
Tiger Louck (RIT Student) <txl9017@ad.rit.edu>
2018-01-26 21:28:33 -0500

    added instructions scene and main menu is scripted.

---
+++ GlobalGameJam2018/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Instructions ()
+    {
+        Debug.Log("chose instructions");
+        SceneManager.LoadScene("Instructions", LoadSceneMode.Single);
+    }
+
+    public void GoExplorer ()
+    {
+        Debug.Log("chose Explorer");
+    }
+
+    public void GoResearcher ()
+    {
+        Debug.Log("chose researcher");
+    }
+}

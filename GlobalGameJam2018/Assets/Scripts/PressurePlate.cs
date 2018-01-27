16a35b347aa57dbe6fdc4e9459b8b4e72ed0acf6
Tiger Louck (RIT Student) <txl9017@ad.rit.edu>
2018-01-26 23:16:48 -0500

    Made the crate and plate puzzle

---
+++ GlobalGameJam2018/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+    public GameObject puzzleMaster;
+    public int plateCode;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Interact()
+    {
+        puzzleMaster.SendMessage("HitPlate", plateCode);
+    }
+
+    private void OnMouseDown()
+    {
+        Interact();
+    }
+}
